e9f237bb285f74299f87d1b5b6fdb076c2e1345e
Cristian Contreras <contreras.cristian994@gmail.com>
2024-02-03 22:32:11 -0500

    [signup]: signup hashing works

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from cs50 import SQL
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+db = SQL("sqlite:///database.db")
 
 @app.route("/")
 def index():
@@ -16,7 +20,26 @@ def login():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username or not password or not confirmation:
+            return render_template("signup.html", error="Please fill in the form")
+        elif password != confirmation:
+            return render_template("signup.html", error="Passwords do not match")
+        data = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if data:
+            return render_template("signup.html", error="Username already exists")
+        hashed = generate_password_hash(password)
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hashed)
+        data = db.execute("SELECT * FROM users WHERE username = ?", username)
+        session["user_id"] = data[0]["id"]
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
 
 
 if __name__ == '__main__':
