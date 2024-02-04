1bec916370d708b55039cf50ba150f1f51d92b8f
Cristian Contreras <contreras.cristian994@gmail.com>
2024-02-03 20:43:11 -0500

    [signin]: added signin/signup

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
@@ -7,6 +9,14 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == '__main__':
